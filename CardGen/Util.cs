0f22a2ffa8c2f3dca652d226a3de00868ae568ca
John Mazouri <jmazouri@users.noreply.github.com>
2014-04-23 08:38:09 -0400

    Enhanced performance of card scaling Added ability to change font of title and description Added ability to export card image

---
+++ CardGen/Util.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CardGen
+{
+    public class Util
+    {
+        public static void CopyRegionIntoImage(Bitmap srcBitmap, Rectangle srcRegion, ref Bitmap destBitmap, Rectangle destRegion)
+        {
+            using (Graphics grD = Graphics.FromImage(destBitmap))
+            {
+                grD.DrawImage(srcBitmap, destRegion, srcRegion, GraphicsUnit.Pixel);
+            }
+        }
+
+        public static Bitmap ResizeBitmap(Bitmap sourceBMP, double width, double height)
+        {
+            Bitmap result = new Bitmap((int)width, (int)height);
+            result.MakeTransparent();
+            using (Graphics g = Graphics.FromImage(result))
+                g.DrawImage(sourceBMP, 0, 0, (int)width, (int)height);
+            return result;
+        }
+    }
+}
